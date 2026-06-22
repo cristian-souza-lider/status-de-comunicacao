2c37b105bdf9160a5772d1a151fc89d9d410f274
Cristian Souza <cristian.souza@gruponso.com.br>
2026-06-22 11:34:22 -0300

    Removido webdriver-manager para execucao offline

--- robot/web_robot.py
+++ robot/web_robot.py
@@ -6,7 +6,6 @@ import shutil
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -166,8 +165,8 @@ def executar_robot():
     }
     chrome_options.add_experimental_option("prefs", prefs)
 
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
+    
     driver.maximize_window()
     
     try:
