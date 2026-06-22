2a366e0a53f2d0afbc0585c7e2a618c167ec27e4
Cristian Souza <cristian.souza@gruponso.com.br>
2026-06-22 12:09:09 -0300

    Ativado modo headless para contornar seguranca corporativa

--- robot/web_robot.py
+++ robot/web_robot.py
@@ -150,6 +150,7 @@ def executar_robot():
     os.makedirs(temp_download_dir)
 
     chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
